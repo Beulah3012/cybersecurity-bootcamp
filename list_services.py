ea06f2a736fb61b05642799dcfb1900dd2817812
Kemi Opata <hephzibah2402@gmail.com>
2025-08-27 19:55:38 -0700

    list_services.py

---
+++ list_services.py
@@ -0,0 +1,21 @@
+# Write a Python script to list running services, reinforcing service management.
+# Task:
+# Script to:
+# List running Windows services.
+# Print service names and statuses.
+# Handle errors (e.g., permission issues).
+# Hints:
+# Use winreg module to access registry.
+# Run in Windows VM as AdminUser.
+
+
+
+import os
+
+try:
+    print("=== running services ===")
+    os.system("sc query state= all")
+except Exception as e:
+    print("Error:",e)
+
+
